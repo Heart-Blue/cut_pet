c20568159e0be17a1aef7c1dfad4a76b088e8edc
Heart-Blue <642732850@qq.com>
2022-06-28 10:35:44 +0800

    feat：配置ios端的权限配置

--- README.md
+++ README.md
@@ -1,16 +1,63 @@
 # cute_pet
 
-A new Flutter project.
+采用getx的路由，状态管理，依赖管理，多语言和主题切换的demo项目
 
-## Getting Started
+支持Android和iOS的双端演示与运行
 
-This project is a starting point for a Flutter application.
+flutter版本：3.0.0  
+dart版本：2.12.0
 
-A few resources to get you started if this is your first Flutter project:
+## app的登录
+登录名：123  
+密码：123
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+## 项目中使用到的插件
+```c
+# http请求器
+dio: ^4.0.6
+# dio拦截器
+pretty_dio_logger: ^1.2.0-beta
+# 本地存储
+shared_preferences: ^2.0.7
+# 上拉刷新下拉加载
+pull_to_refresh: ^2.0.0
+# 日志打印
+logger: ^1.1.0
+# getx
+get: ^4.6.1
+# 屏幕适配
+flutter_screenutil: ^5.5.3+1
+# 懒加载的层叠组件
+proste_indexed_stack: ^0.2.4
+# 加载中
+flutter_easyloading: ^3.0.3
+# 网络图片缓存
+cached_network_image: ^3.2.0
+# 瀑布流插件
+flutter_staggered_grid_view: ^0.4.1
+# flutter选择器
+flutter_picker: ^2.0.3
+# 省市区三级联动
+city_pickers: ^1.0.1
+# 轮播图
+flutter_swiper_null_safety: ^1.0.2
+# 显示html
+flutter_widget_from_html_core: ^0.8.5+3
+# 选择时视频播放器
+video_player: ^2.2.2
+# 权限配置
+permission_handler: ^9.0.2
+# 图片保存
+image_gallery_saver: ^1.7.1
+# 轮播
+carousel_slider: ^4.1.1
+# 图片查看器（预览）
+photo_view: ^0.13.0
+# app图标
+flutter_launcher_icons: ^0.9.2
+# 闪屏页
+flutter_native_splash: ^1.2.0
+```
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## 注意
+请勿随意添加或者删除数据，添加的评论信息记得及时删除。
--- lib/components/gallery_photo_view.dart
+++ lib/components/gallery_photo_view.dart
@@ -1,10 +1,13 @@
 import 'dart:io';
+import 'dart:typed_data';
 import 'package:cute_pet/utils/find_config.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:photo_view/photo_view_gallery.dart';
 
@@ -50,140 +53,137 @@ class _PhotoViewGalleryScreenState extends State<PhotoViewGalleryScreen> {
         value: SystemUiOverlayStyle.light,
         child: Scaffold(
             body: Stack(
-              children: <Widget>[
-                Container(
-                  width: Get.width,
-                  height: Get.height,
-                  color: Colors.black,
-                  child: PhotoViewGallery.builder(
-                    scrollPhysics: const BouncingScrollPhysics(),
-                    builder: (BuildContext context, int index) {
-                      dynamic tmpImage;
-                      if (widget.type == ImageType.net) {
-                        tmpImage = NetworkImage(widget.images![index]);
-                      } else if (widget.type == ImageType.asset) {
-                        tmpImage = AssetImage(widget.images![index]);
-                      } else if (widget.type == ImageType.file) {
-                        tmpImage = FileImage(File(widget.images![index].path));
-                      } else {
-                        tmpImage = NetworkImage(widget.images![index]);
-                      }
-                      return PhotoViewGalleryPageOptions(
-                        imageProvider: tmpImage,
-                      );
-                    },
-                    itemCount: widget.images!.length,
-                    backgroundDecoration: null,
-                    pageController: controller,
-                    enableRotation: false,
-                    onPageChanged: (index) {
-                      setState(() {
-                        currentIndex = index;
-                      });
-                    },
-                  ),
+          children: <Widget>[
+            Container(
+              width: Get.width,
+              height: Get.height,
+              color: Colors.black,
+              child: PhotoViewGallery.builder(
+                scrollPhysics: const BouncingScrollPhysics(),
+                builder: (BuildContext context, int index) {
+                  dynamic tmpImage;
+                  if (widget.type == ImageType.net) {
+                    tmpImage = NetworkImage(widget.images![index]);
+                  } else if (widget.type == ImageType.asset) {
+                    tmpImage = AssetImage(widget.images![index]);
+                  } else if (widget.type == ImageType.file) {
+                    tmpImage = FileImage(File(widget.images![index].path));
+                  } else {
+                    tmpImage = NetworkImage(widget.images![index]);
+                  }
+                  return PhotoViewGalleryPageOptions(
+                    imageProvider: tmpImage,
+                  );
+                },
+                itemCount: widget.images!.length,
+                backgroundDecoration: null,
+                pageController: controller,
+                enableRotation: false,
+                onPageChanged: (index) {
+                  setState(() {
+                    currentIndex = index;
+                  });
+                },
+              ),
+            ),
+            Positioned(
+              //图片index显示
+              top: MediaQuery.of(context).padding.top + 15,
+              width: MediaQuery.of(context).size.width,
+              child: Center(
+                child: Text("${currentIndex + 1}/${widget.images!.length}",
+                    style: const TextStyle(color: Colors.white, fontSize: 16)),
+              ),
+            ),
+            Positioned(
+              //右上角关闭按钮
+              right: 10,
+              top: MediaQuery.of(context).padding.top,
+              child: IconButton(
+                icon: const Icon(
+                  Icons.close,
+                  size: 30,
+                  color: Colors.white,
                 ),
-                Positioned(
-                  //图片index显示
-                  top: MediaQuery.of(context).padding.top + 15,
-                  width: MediaQuery.of(context).size.width,
-                  child: Center(
-                    child: Text("${currentIndex + 1}/${widget.images!.length}",
-                        style:
-                            const TextStyle(color: Colors.white, fontSize: 16)),
-                  ),
-                ),
-                Positioned(
-                  //右上角关闭按钮
-                  right: 10,
-                  top: MediaQuery.of(context).padding.top,
-                  child: IconButton(
-                    icon: const Icon(
-                      Icons.close,
-                      size: 30,
-                      color: Colors.white,
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ),
+            // 保存图片部件
+            Positioned(
+                bottom: MediaQuery.of(context).padding.bottom + 16,
+                right: 10,
+                child: Visibility(
+                  visible: widget.type == ImageType.net,
+                  child: TextButton(
+                    child: const Text(
+                      '保存图片',
+                      style: TextStyle(color: Colors.white, fontSize: 16),
                     ),
-                    onPressed: () {
-                      Navigator.of(context).pop();
+                    onPressed: () async {
+                      bool permission = await requestPermission();
+                      permission
+                          ? saveNetworkImg(widget.images![currentIndex])
+                          : null;
                     },
                   ),
-                ),
-                // 保存图片部件
-                // Positioned(
-                //     bottom: MediaQuery.of(context).padding.bottom + 16,
-                //     right: 10,
-                //     child: Visibility(
-                //       visible: widget.type == ImageType.net,
-                //       child: TextButton(
-                //         child: Text(
-                //           '保存图片',
-                //           style: TextStyle(color: Colors.white, fontSize: 16),
-                //         ),
-                //         onPressed: () {
-                //           if (Platform.isIOS) {
-                //             return _savenNetworkImage();
-                //           }
-                //           requestPermission().then((bool) {
-                //             if (bool) {
-                //               _savenNetworkImage();
-                //             }
-                //           });
-                //         },
-                //       ),
-                //     ))
-              ],
-            )));
+                ))
+          ],
+        )));
   }
 
   // 动态申请权限，ios 要在info.plist 上面添加
   Future<bool> requestPermission() async {
-    var status = await Permission.photos.status;
-    var status1 = await Permission.storage.status;
-    var status2 = await Permission.camera.status;
-    if (!status.isGranted || !status1.isGranted || !status2.isGranted) {
-      [
-        Permission.photos,
-        Permission.storage,
-        Permission.camera,
-      ].request();
+    late PermissionStatus status;
+    if (Platform.isIOS) {
+      status = await Permission.photosAddOnly.request();
+    } else {
+      status = await Permission.storage.request();
     }
-    return status.isGranted && status1.isGranted && status2.isGranted;
+    if (status != PermissionStatus.granted) {
+      showCupertinoDialog(
+          context: context,
+          builder: (context) {
+            return CupertinoAlertDialog(
+              title: const Text('You need to grant album permissions'),
+              content: const Text(
+                  'Please go to your mobile phone to set the permission to open the corresponding album'),
+              actions: <Widget>[
+                CupertinoDialogAction(
+                  child: const Text('cancle'),
+                  onPressed: () {
+                    Get.back();
+                  },
+                ),
+                CupertinoDialogAction(
+                  child: const Text('confirm'),
+                  onPressed: () {
+                    Get.back();
+                    openAppSettings();
+                  },
+                ),
+              ],
+            );
+          });
+    } else {
+      return true;
+    }
+    return false;
   }
 
-  // /检查权限
-  // Future<bool> _checkPermission() async {
-  //   if (Theme.of(context).platform == TargetPlatform.android) {
-  //     final status = await Permission.storage.status;
-  //     if (status != PermissionStatus.granted) {
-  //       final result = await Permission.storage.request();
-  //       if (result == PermissionStatus.granted) {
-  //         return true;
-  //       }
-  //     } else {
-  //       return true;
-  //     }
-  //   }
-  //   return false;
-  // }
-
-  //保存网络图片到本地
-  // _savenNetworkImage() async {
-  //   var response = await Dio().get(widget.images![widget.index],
-  //       options: Options(responseType: ResponseType.bytes));
-  //   final result =
-  //       await ImageGallerySaver.saveImage(Uint8List.fromList(response.data));
-  //   if (Platform.isIOS) {
-  //     if (result['filePath'] != null) {
-  //       EasyLoading.showSuccess("保存成功，请前往 '相册' 查看");
-  //     } else {
-  //       EasyLoading.showError('保存失败');
-  //     }
-  //   } else {
-  //     if (result['filePath'] != null) {
-  //       EasyLoading.showSuccess("保存成功，请前往 '相册' 查看");
-  //     } else {
-  //       EasyLoading.showError('保存失败');
-  //     }
-  //   }
-  // }
+  // 保存网络图片
+  saveNetworkImg(String imgUrl) async {
+    var response = await Dio()
+        .get(imgUrl, options: Options(responseType: ResponseType.bytes));
+    final result = await ImageGallerySaver.saveImage(
+      Uint8List.fromList(response.data),
+      quality: 100,
+    );
+    if (result['isSuccess']) {
+      EasyLoading.showSuccess("保存成功，请前往 '相册' 查看");
+    } else {
+      EasyLoading.showError('保存失败');
+    }
+  }
 }
